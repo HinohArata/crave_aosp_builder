9a3d244dffc885a9c61933b39629d9b3c255a030
HinohArata <mmgcntk@gmail.com>
2024-10-13 19:31:06 +0700

    project: Add ttp build script
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

---
+++ project/ttp_script.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Setup URL
+GIT="https://github.com"
+LAB="https://gitlab.com"
+ARATA="HinohArata"
+ARATA1="Arata-Stuffs"
+
+# Setup directory
+LOCAL=".repo/local_manifests"
+
+if repo init --depth=1 -u $GIT/The-Pixel-Project/manifest.git -b fourteen-qpr3 --git-lfs; then
+     echo -e "================================"
+     echo -e "=      Build starting...       ="
+     echo -e "================================"
+     echo -e "=  Repo has been initialized   ="
+     echo -e "================================"
+     if [ -d "$LOCAL" ]; then
+         rm -rf $LOCAL/*
+         if wget -O "$LOCAL/local_manifest.xml" https://raw.githubusercontent.com/$ARATA1/local_manifest/refs/heads/master/local_manifest_ttp.xml; then
+             echo -e "================================"
+             echo -e "=  Local manifest initialized  ="
+             echo -e "================================"
+
+             # Set up build environment
+             /opt/crave/resync.sh; export BUILD_USERNAME=HinohArata; export BUILD_HOSTNAME=crave; export TZ=Asia/Jakarta
+
+             # Build rom
+             source build/envsetup.sh; lunch aosp_surya-user; make installclean; make bacon
+         else
+             echo -e "================================"
+             echo -e "= Failed to iniatylizing local ="
+             echo -e "================================"
+         fi
+     else
+         echo -e "$LOCAL not found, no need to remove"
+         mkdir -p $LOCAL
+         if wget -O "$LOCAL/local_manifest.xml" https://raw.githubusercontent.com/$ARATA1/local_manifest/refs/heads/master/local_manifest_ttp.xml; then
+             echo -e "================================"
+             echo -e "=  Local manifest initialized  ="
+             echo -e "================================"
+
+             # Set up build environment
+             /opt/crave/resync.sh; export BUILD_USERNAME=HinohArata; export BUILD_HOSTNAME=crave; export TZ=Asia/Jakarta
+
+             # Build rom
+             source build/envsetup.sh; lunch aosp_surya-user; make installclean; make bacon
+         else
+             echo -e "================================"
+             echo -e "= Failed to iniatylizing local ="
+             echo -e "================================"
+         fi
+     fi
+else
+     echo -e "================================"
+     echo -e "=         Build Failed         ="
+     echo -e "================================"
+fi
